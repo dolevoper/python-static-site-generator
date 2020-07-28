9af3697d7ee21371af25ee58b07b50abd2f27e9b
Omer Dolev <odolev@microsoft.com>
2020-07-28 11:06:10 +0300

    fix dictionary

--- ssg.py
+++ ssg.py
@@ -4,9 +4,9 @@ from ssg.site import Site
 
 def main(source = "content", dest = "dist"):
     config = {
-        source: source,
-        dest: dest,
-        parsers: [ssg.parsers.ResourceParser(), ssg.parsers.MarkdownParser(), ssg.parsers.ReStructuredTextParser()]
+        "source": source,
+        "dest": dest,
+        "parsers": [ssg.parsers.ResourceParser(), ssg.parsers.MarkdownParser(), ssg.parsers.ReStructuredTextParser()]
     }
     Site(**config).build()
 
